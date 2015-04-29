16d71dd3992923a56fc3a11680780ebadcc6c368
Alex Vargas <vargas.84@osu.edu>
2015-04-28 22:25:06 -0400

    Configure rails environments. Add Stage.

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -13,8 +13,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Route emails thorugh Postfix in stage
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
